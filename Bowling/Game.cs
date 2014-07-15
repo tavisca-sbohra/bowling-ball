49a07c25f21193ab5a2d6bdadfad7744a1560007
sbohra <sbohra@gmail.com>
2014-07-15 19:41:03 +0530

    Shivangi Final Commit

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -8,15 +8,79 @@ namespace Bowling
 {
     public class Game
     {
+
+        public int latestscore=0;
+        public   int calledtimes=0;
+        public int[] allpin=new int[30];
+
+        
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            
+            if (pins < 10)
+            {
+                //add it to the array ;
+                //he gets another chance to roll 
+             
+                allpin[calledtimes] = pins;
+                calledtimes =calledtimes + 1;
+ 
+            }
+            if (pins == 10)
+            {
+                //add it the array
+                allpin[calledtimes] = pins;
+                allpin[calledtimes + 1] = 0;
+                calledtimes += 2;
+            }
+           
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
-        }
 
+            //caculate the latest score here 
+            int i;
+            
+            for (i = 0; i < 20; i++)
+            {
+                latestscore += allpin[i];
+            }
+            for (i = 0; i < 19; i += 2)
+            {
+                if (allpin[i] == 10 && allpin[i + 2] == 10 )
+                    latestscore += 10 + allpin[i+4];
+                else if(allpin[i]==10 && allpin[i+2]!=10)
+                    latestscore += allpin[i + 2] + allpin[i + 3];
+            }
+
+            //if (allpin[16] == 10 && allpin[18] == 10)
+            //{
+            //    latestscore += 10 + allpin[20];
+            //}
+            //if (allpin[18] == 10 )
+            //{
+            //    latestscore += allpin[21]+ allpin[20];
+            //}
+            //for (i = 1; i < 19; i+=2)
+            //{
+            //    if (allpin[i] == 10)
+            //    {
+            //        latestscore += allpin[i + 1];
+            //    }
+            //}
+
+            for (i = 0; i < 20; i += 2)
+            {
+                if ((allpin[i] + allpin[i + 1] == 10) && (allpin[i] != 10 ))
+                {
+                    latestscore += allpin[i + 2];
+                }
+            }
+                return latestscore;
+          
+        }  //getscore ends here
+
+        
     }
 }
--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Bowling;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BowlingFixtures
@@ -7,8 +8,201 @@ namespace BowlingFixtures
     public class GameFixture
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SomeValuesOnly()
         {
+           Game o = new Game();
+           o.Roll(10);
+           o.Roll(10);
+           o.Roll(10);
+
+           System.Diagnostics.Debug.WriteLine("value :" + o.GetScore());
+            
         }
-    }
+
+
+        [TestMethod]
+        public void calculateScoreInBetween()
+        {
+            Game o = new Game();
+            o.Roll(10);
+            o.Roll(10);
+            o.Roll(1);
+            o.Roll(6);
+            o.Roll(3);
+            o.Roll(0);
+            
+           
+            Assert.AreEqual(48, o.GetScore());
+           
+            
+        }
+
+        [TestMethod]
+        public void Gutter()
+         {
+        Game o = new Game();
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        o.Roll(0);
+        Assert.AreEqual(0, o.GetScore());
+         }
+ 
+         
+         [TestMethod]
+         public void NoSpareNoStrike()
+         {
+             Game o = new Game();
+             o.Roll(1);
+            o.Roll(2);
+
+             o.Roll(3);
+             o.Roll(4);
+
+             o.Roll(5);
+             o.Roll(4);
+
+             o.Roll(3);
+             o.Roll(2);
+
+             o.Roll(1);
+             o.Roll(0);
+
+             o.Roll(1);
+             o.Roll(2);
+
+             o.Roll(3);
+             o.Roll(4);
+
+             o.Roll(5);
+             o.Roll(4);
+
+             o.Roll(3);
+             o.Roll(2);
+
+             o.Roll(0);
+             o.Roll(1);
+
+             Assert.AreEqual(50, o.GetScore());
+            
+         }
+
+         [TestMethod]
+         public void OnlySpareAllRandom()
+         {
+             Game o = new Game();
+             o.Roll(1);
+             o.Roll(9);
+
+             o.Roll(1);
+             o.Roll(0);
+
+             o.Roll(3);
+             o.Roll(7);
+
+             o.Roll(2);
+             o.Roll(0);
+
+             o.Roll(5);
+             o.Roll(5);
+
+             o.Roll(3);
+             o.Roll(0);
+
+             o.Roll(7);
+             o.Roll(3);
+
+             o.Roll(4);
+             o.Roll(0);
+
+             o.Roll(9);
+             o.Roll(0);
+
+             o.Roll(0);
+             o.Roll(1);
+
+
+             System.Diagnostics.Debug.WriteLine("value :" + o.GetScore());
+         }
+
+
+         
+         [TestMethod]
+         public void AllStrike()
+         {
+             Game o = new Game();
+             o.Roll(10);
+             o.Roll(10);
+
+             o.Roll(10);
+             o.Roll(10);
+
+             o.Roll(10);
+             o.Roll(10);
+
+             o.Roll(10);
+             o.Roll(10);
+
+             o.Roll(10);
+             o.Roll(10);
+
+             o.Roll(10);
+             o.Roll(10);
+
+             System.Diagnostics.Debug.WriteLine("value :" +o.GetScore());
+         }
+
+         [TestMethod]
+         public void RandomValues()
+         {
+             Game o = new Game();
+             o.Roll(3);
+             o.Roll(6);
+
+             o.Roll(10);
+
+             o.Roll(4);
+             o.Roll(5);
+
+             o.Roll(5);
+             o.Roll(5);
+
+             o.Roll(1);
+             o.Roll(1);
+
+             o.Roll(10);
+
+             o.Roll(6);
+             o.Roll(4);
+
+             o.Roll(10);
+
+             o.Roll(0);
+             o.Roll(10);
+
+             o.Roll(10);
+
+             o.Roll(0);
+             o.Roll(10);
+
+             System.Diagnostics.Debug.WriteLine("value :" + o.GetScore());
+         }
+      }
 }
